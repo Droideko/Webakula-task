916265a682a923f6f7839bb2e52f524a0742665d
Droideka <vindou@mail.ru>
2019-06-17 14:03:40 +0300

    one

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper('.swiper-container', {
+   slidesPerView: 3,
+   spaceBetween: 80,
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+ });
